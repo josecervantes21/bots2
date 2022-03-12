a51e9b89d7cc09eacd28d9822ecd2699d711c797
josecervantes21 <95323111+josecervantes21@users.noreply.github.com>
2022-03-12 11:40:40 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,53 +1,33 @@
-const { Client, Intents,MessageEmbed } = require('discord.js');
+const { Client, Intents, NewsChannel, } = require('discord.js');
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
-client.on('ready',()=>{
-    console.log(`Bot is ready as: ${client.user.tag}`);
-});
+//Horas y palabras clave
+var horas=['12','13','14','15','16','17','18','19','20']  
+var contenido=['retos','explicar','expliquenme']
 
-var datamet=[]
-var dataname=[]
-var database=[]
-var clock=[]
 client.on("message", message=>{
-    if(message.content==='hola'){
-        message.reply(`¡Hola ${message.author}! ¿De que forma aprendes`)
+    //condición de entrada
+     switch (message.content.toLowerCase()) {
+        case 'hola': 
+            message.reply(`Hola, como te gusta aprender ${message.author}`)
+            break;
+     }
+     //recorrido de listas y validación
+     for (let i = 0; i < horas.length; i++)
+     {
+     switch (message.content.toLowerCase()) {
+        case contenido[i]:
+            message.reply('¿A qué hora estás disponible?')
+            message.author.selection=(`${contenido[i]}`)
+            break;
+        case horas[i]:
+            message.author.availability=horas[i]
+            message.reply('Te avisaré cuando haya un match')
+            console.log(message.author)
+            break;
     }
-    
 
-    if(message.content.includes("retos")){
-        var nombre=message.author
-        dataname.push(nombre)
-        var challengues='retos'
-        datamet.push(challengues)
-
-    var person1={
-        "name":`${dataname}`,
-        "met":`${datamet}`
-    } 
-
-    database.push(person1)
-    console.log(database)
-
-    var coincidencias=[]
-    coincidencias.push([])
-    
-    for (let i = 0; i < datamet.length; i++) {
-        if(i === 0){
-            if(message.author.bot) return
-            message.reply(`Te avisare cuando haya un match`)
-            console.log(i)
-        }
-        else if(i === 1){
-            if(message.author.bot) return
-            message.reply(`Hey mira lo que encontré, ${dataname} disfruta aprender por medio de -retos-, lo cual empata muy bien contigo. Podrían juntarse a las -6:00 pm- a -hacer retos en code wars-.`)
-            console.log(i)
-            break
-        }
-        
-    }
 }
 })
 
-
-client.login('OTQ1MTkzNTM1NDgzODMwMzMy.YhMmEw.DTvMGmhejNliQey9N67Hw2uMvdU');
+client.login('OTQ1MTkzNTM1NDgzODMwMzMy.YhMmEw.ISAq1PGm6uvsVysu5OEHJArM6lA');
